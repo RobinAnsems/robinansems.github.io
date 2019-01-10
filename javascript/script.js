3a41c103a939c00a35aea1d3e971747c0ebdf02a
RobinAnsems <45545623+RobinAnsems@users.noreply.github.com>
2019-01-10 23:34:18 +0100

    10 jan 23:34

--- javascript/script.js
+++ javascript/script.js
@@ -25,20 +25,37 @@ slider.oninput = function () {
 };
 
 
+/*Veranderen van hartje bron: https://stackoverflow.com/questions/34102091/changing-images-src-with-event-onclick*/
+var foto1 =  document.getElementById("favorieten");
 
-
-
-
-var image =  document.getElementById("favorieten");
-
-            function veranderHartje()
-            {
-                if (image.getAttribute('src') == "images/favoriet.svg")
-                {
-                    image.src = "images/gefavoriet.svg";
+            function veranderHartje() {
+                if (foto1.getAttribute('src') == "images/favoriet.svg") {
+                    foto1.src = "images/gefavoriet.svg";
                 }
-                else
-                {
-                    image.src = "images/favoriet.svg";
+                else {
+                    foto1.src = "images/favoriet.svg";
                 }
-            }
+            };
+
+var foto2 =  document.getElementById("notificatie");
+
+            function deNotificatie() {
+                if (foto2.getAttribute('src') == "images/favoriet-nav.svg") {
+                    foto2.src = "images/notificatie-2.svg";
+                }
+                else {
+                    foto2.src = "images/favoriet-nav.svg";
+                }
+            };
+
+
+/*niet scrolbare website na klikken van link bron: https://stackoverflow.com/questions/1601933/how-do-i-stop-a-web-page-from-scrolling-to-the-top-when-a-link-is-clicked-that-t*/
+$('#favorieten').click(function ($e) {
+    $e.preventDefault();
+    doSomething();
+});
+
+document.getElementById('#favorieten').addEventListener('click', function (e) {
+    e.preventDefault();
+    doSomething();
+});
