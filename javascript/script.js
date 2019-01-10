0e4fbc0d86e213a1d5a2e8a6bd67a3b8f42186d0
RobinAnsems <45545623+RobinAnsems@users.noreply.github.com>
2019-01-10 19:32:09 +0100

    10 jan 19:32

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,21 @@
 /*FED JS Robin Ansems studentnummer:500780651 RET Website*/
 
+
+/*filter- en sorteeropties in- en uitklapbaar bron: https://www.youtube.com/watch?v=sxZhsBLg2Ls*/
+$('#showformulier').click(function () {
+    $('#showformulier').css('display', 'none');
+    $('#formulier').show();
+    $('#hideformulier').show();
+});
+
+$('#hideformulier').click(function () {
+	$('#hideformulier').css('display', 'none');
+	$('#formulier').hide();
+	$('#showformulier').show();
+});
+
+
+/*Slider bron: https://www.w3schools.com/howto/howto_js_rangeslider.asp*/
 var slider = document.getElementById("slider1");
 var output = document.getElementById("nummer");
 output.innerHTML = slider.value;
@@ -7,3 +23,22 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
   output.innerHTML = this.value + " minuten";
 };
+
+
+
+
+
+
+var image =  document.getElementById("favorieten");
+
+            function veranderHartje()
+            {
+                if (image.getAttribute('src') == "images/favoriet.svg")
+                {
+                    image.src = "images/gefavoriet.svg";
+                }
+                else
+                {
+                    image.src = "images/favoriet.svg";
+                }
+            }
