0b2fa3627145c96da78f2039c9b7c6ae3773abe4
RobinAnsems <robinansems00@gmail.com>
2020-02-22 14:07:33 +0100

    22 februari 2020
    cursus css erbij

---
+++ images/demo/CMD3/cursuscss/javascript/script.js
@@ -0,0 +1,175 @@
+function falconheavy() {
+  document.querySelector(".falconheavy").className = "falconheavy";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".falconheavy").className = "falconheavy falconheavygo"; }); }); }
+document.querySelector(".button").addEventListener("click", falconheavy, false);
+
+
+
+function vuur() {
+  document.querySelector(".vuur").className = "vuur";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".vuur").className = "vuur vuurgo"; }); }); }
+document.querySelector(".button").addEventListener("click", vuur, false);
+
+
+
+function rook1() {
+  document.querySelector(".rook1").className = "rook1";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".rook1").className = "rook1 rook1go"; }); }); }
+document.querySelector(".button").addEventListener("click", rook1, false);
+
+
+
+function rook2() {
+  document.querySelector(".rook2").className = "rook2";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".rook2").className = "rook2 rook2go"; }); }); }
+document.querySelector(".button").addEventListener("click", rook2, false);
+
+
+
+function rook3() {
+  document.querySelector(".rook3").className = "rook3";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".rook3").className = "rook3 rook3go"; }); }); }
+document.querySelector(".button").addEventListener("click", rook3, false);
+
+
+
+function rook() {
+  document.querySelector(".rook").className = "rook";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".rook").className = "rook rookgo"; }); }); }
+document.querySelector(".button").addEventListener("click", rook, false);
+
+
+
+function grondweg() {
+  document.querySelector(".grondweg").className = "grondweg";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".grondweg").className = "grondweg grondweggo"; }); }); }
+document.querySelector(".button").addEventListener("click", grondweg, false);
+
+
+
+function luchtveranderen() {
+  document.querySelector(".luchtveranderen").className = "luchtveranderen";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".luchtveranderen").className = "luchtveranderen luchtveranderengo"; }); }); }
+document.querySelector(".button").addEventListener("click", luchtveranderen, false);
+
+
+
+function nummer1() {
+  document.querySelector(".nummer1").className = "nummer1";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".nummer1").className = "nummer1 nummer1go"; }); }); }
+document.querySelector(".button").addEventListener("click", nummer1, false);
+
+
+
+function nummer2() {
+  document.querySelector(".nummer2").className = "nummer2";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".nummer2").className = "nummer2 nummer2go"; }); }); }
+document.querySelector(".button").addEventListener("click", nummer2, false);
+
+
+function nummer3() {
+  document.querySelector(".nummer3").className = "nummer3";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".nummer3").className = "nummer3 nummer3go"; }); }); }
+document.querySelector(".button").addEventListener("click", nummer3, false);
+
+
+
+function nummer4() {
+  document.querySelector(".nummer4").className = "nummer4";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".nummer4").className = "nummer4 nummer4go"; }); }); }
+document.querySelector(".button").addEventListener("click", nummer4, false);
+
+
+function nummer5() {
+  document.querySelector(".nummer5").className = "nummer5";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".nummer5").className = "nummer5 nummer5go"; }); }); }
+document.querySelector(".button").addEventListener("click", nummer5, false);
+
+
+
+function liftoff() {
+  document.querySelector(".liftoff").className = "liftoff";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".liftoff").className = "liftoff liftoffgo"; }); }); }
+document.querySelector(".button").addEventListener("click", liftoff, false);
+
+
+
+function wolken() {
+  document.querySelector(".wolken").className = "wolken";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".wolken").className = "wolken wolkengo"; }); }); }
+document.querySelector(".button").addEventListener("click", wolken, false);
+
+
+
+function mars() {
+  document.querySelector(".mars").className = "mars";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".mars").className = "mars marsgo"; }); }); }
+document.querySelector(".button").addEventListener("click", mars, false);
+
+
+
+function armrechts() {
+  document.querySelector(".armrechts").className = "armrechts";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".armrechts").className = "armrechts armrechtsgo"; }); }); }
+document.querySelector(".button").addEventListener("click", armrechts, false);
+
+
+
+function alien() {
+  document.querySelector(".alien").className = "alien";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".alien").className = "alien aliengo"; }); }); }
+document.querySelector(".button").addEventListener("click", alien, false);
+
+
+
+function kopje() {
+  document.querySelector(".kopje").className = "kopje";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".kopje").className = "kopje kopjego"; }); }); }
+document.querySelector(".button").addEventListener("click", kopje, false);
+
+
+
+function tekstwolkje() {
+  document.querySelector(".tekstwolkje").className = "tekstwolkje";
+  window.requestAnimationFrame(function(time) {
+    window.requestAnimationFrame(function(time) {
+      document.querySelector(".tekstwolkje").className = "tekstwolkje tekstwolkjego"; }); }); }
+document.querySelector(".button").addEventListener("click", tekstwolkje, false);
